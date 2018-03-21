986c12b42c3e52e37821371451a70d23a0d5fd42
beyond-GH <beyondaverage0908@gmail.com>
2018-03-21 14:17:02 +0800

    Update vhs-modal.js
    beautify show modal and add background cover

--- vhs-modal.js
+++ vhs-modal.js
@@ -3,6 +3,7 @@ vhs = {
      *
      * vhs.showToast({
      *       content: "toast内容",
+     *       duration: 2500,
      *       complete: function() {
      *           console.log("toast消失回调");
      *       }
@@ -43,6 +44,11 @@ vhs = {
             v.parentNode.removeChild(v);
         };
 
+        var duration = 3000;
+        if (toast.duration) {
+            duration = toast.duration;
+        }
+
         if (toast.content) {
             createToastTemplate();
             var x = document.getElementById("vhs-main-toast");
@@ -51,7 +57,7 @@ vhs = {
             setTimeout(function(){
                 removeToast();
                 if (toast.complete) toast.complete();
-            }, 3000);
+            }, duration);
         }
     },
     getValidMetaWidthScale: function() {
@@ -103,73 +109,103 @@ vhs = {
             var scale = that.getValidMetaWidthScale();
 
             var body = document.getElementsByTagName("body")[0];
+
+            var bgCover = document.createElement("div");
+            bgCover.id = "vhs-modal-bg-cover";
+            bgCover.style.backgroundColor = "rgba(51, 51, 51, 0.85)";
+            bgCover.style.width = "100%";
+            bgCover.style.height = "100%";
+            bgCover.style.position = "fixed";
+            bgCover.style.zIndex = "999";
+            bgCover.style.top = "0";
+            body.appendChild(bgCover);
+
             var warp = document.createElement("div");
             warp.id = "vhs-modal-warp";
             warp.style.visibility = "visible";
             warp.style.minWidth = "50%";
             warp.style.maxWidth = "66.8%";
-            warp.style.backgroundColor = "#f0f0f0";
+            warp.style.backgroundColor = "#fff";
             warp.style.color = "#333";
             warp.style.textAlign = "center";
             warp.style.borderRadius = 5 * scale + "px";
-            warp.style.padding = 10 * scale + "px";
+            warp.style.paddingTop = 10 * scale + "px";
             warp.style.position = "fixed";
             warp.style.zIndex = "10";
             warp.style.left = 0;
             warp.style.right = 0;
             warp.style.marginLeft = "auto";
             warp.style.marginRight = "auto";
-            warp.style.bottom = "50%";
+            warp.style.top = "44%";
             warp.style.fontSize = 15 * scale + "px";
             warp.style.borderStyle = "solid";
             warp.style.borderColor = "#e1e0e4";
-            warp.style.borderWidth = "1px";
-            body.appendChild(warp);
+            warp.style.borderWidth = 1 * scale + "px";
+            bgCover.appendChild(warp);
 
             var topDiv = document.createElement("div");
             warp.appendChild(topDiv);
 
             var title = document.createElement("p");
             title.id = "vhs-modal-title";
+            title.style.fontSize = 16 * scale + "px";
+            title.style.marginTop = "0px";
+            title.style.marginBottom = "0px";
             topDiv.appendChild(title);
 
             var content = document.createElement("div");
             content.id = "vhs-modal-content";
-            content.style.paddingTop = 15 * scale + "px";
+            content.style.paddingTop = 10 * scale + "px";
             content.style.paddingBottom = 15 * scale + "px";
+            content.style.paddingLeft = 10 * scale + "px";
+            content.style.paddingRight = 10 * scale + "px";
+            content.style.fontSize = 14 * scale + "px";
+            content.style.color = "rgb(95, 92, 92)";
             topDiv.appendChild(content);
 
             var actionDiv = document.createElement("div");
             actionDiv.id = "vhs-modal-action";
             actionDiv.style.bottom = 9 * scale + "px";
             actionDiv.style.width = "100%";
-            actionDiv.style.borderTop = "1px solid white";
+            actionDiv.style.borderTop = "1px solid rgb(191, 190, 190)";
+            actionDiv.style.height = 41 * scale + "px";
             warp.appendChild(actionDiv);
 
             var cancel = document.createElement("div");
             cancel.id = "vhs-modal-cancel";
-            cancel.style.marginTop = 10 * scale + "px";
             cancel.style.float = "left";
             cancel.style.width = "50%";
             cancel.style.color = "red";
+            cancel.style.height = 41 * scale + "px";
+            cancel.style.lineHeight = 41 * scale + "px";
             actionDiv.appendChild(cancel);
 
             var confirm = document.createElement("div");
             confirm.id = "vhs-modal-confirm";
-            confirm.style.marginTop = 10 * scale + "px";
             confirm.style.float = "right";
-            confirm.style.width = "50%";
+            confirm.style.width = "49%";
+            confirm.style.height = 41 * scale + "px";
+            confirm.style.lineHeight = 41 * scale + "px";
+            confirm.style.borderLeft = "1px solid rgb(191, 190, 190)";
             actionDiv.appendChild(confirm);
         };
 
         var removeModal = function() {
-            var w = document.getElementById("vhs-modal-warp");
+            var w = document.getElementById("vhs-modal-bg-cover");
             w.parentNode.removeChild(w);
+        };
+
+        var fixModalPosition = function() {
+            var scale = that.getValidMetaWidthScale();
+            var warpDiv = document.getElementById("vhs-modal-warp");
+            var winOutHeight = window.outerHeight;
+            warpDiv.style.top = (winOutHeight * scale - warpDiv.clientHeight) / 2 + "px";
+            console.log((winOutHeight * scale - warpDiv.clientHeight) / 2 + "px");
         };
 
         if (modal.content) {
             createModalTemplate();
-            // 设置title
+            // default "提示", dynamic set modal title
             var title = document.getElementById("vhs-modal-title");
             title.innerText = modal.title || "提示";
 
@@ -178,6 +214,10 @@ vhs = {
 
             var cancel = document.getElementById("vhs-modal-cancel");
             cancel.innerHTML = modal.cancelTitle || "取消";
+
+            // position fix to window center
+            fixModalPosition();
+
             cancel.onclick = function() {
                 if (modal.cancel) {
                     modal.cancel();
